63d8381719147758fe2bc145b634d0393a9e9533
Nic19907 <88468884+Nic19907@users.noreply.github.com>
2022-04-12 12:41:25 -0600

    Archivo para la configuracion del jugador 1
    Define los botones con interrupciones internas del ESP

---
+++ Controles_jugador1/Controles_jugador1.ino
@@ -0,0 +1,20 @@
+
+struct Boton {
+  const uint8_t PIN;
+  char caracter;
+  bool estado;
+}
+
+Boton up = {25, 
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
