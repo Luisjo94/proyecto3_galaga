6f769861f25304d5c9aab30574a9bbad3271ce14
Nic19907 <88468884+Nic19907@users.noreply.github.com>
2022-04-13 17:08:43 -0600

    Update joy_jug1.ino
    Palanca y boton por separado

--- Controles_jugador1/joy_jug1/joy_jug1.ino
+++ Controles_jugador1/joy_jug1/joy_jug1.ino
@@ -1,49 +1,55 @@
-//************************ Variables de botones **********************************
+//************************ Variables de palanca **********************************
 struct Boton {
   const uint8_t PIN;
   char caracter;
   bool estado;
-  //bool auxiliar;
 };
-
-//se define lo que hace cada boton (pin, character que representa y su estado actual)
-//Boton up = {25, 'W', false};
-//Boton left = {33, 'A', false};
-//Boton down = {32, 'S', false};
-//Boton right = {35, 'D', false};
-Boton shoot = {34, 'E', false};
+Boton shoot = {19, 'E', false};
 
 struct Joystick {
   const uint8_t PIN;
   uint16_t volt;
-  const uint16_t centro;
 };
-//dead zone de 100 en x
-//dead zone de 75 en y
-Joystick ejeX {33, 0, 1095};
-Joystick ejeY {32, 0, 1894};
+Joystick ejeX {33, 0};
+Joystick ejeY {32, 0};
+
+void IRAM_ATTR ISR(){
+
+  shoot.estado = true;
+
+}
 
 void setup() {
   Serial.begin(115200);
-  
   input_setup();
-  //interrupt_setup();
+  attachInterrupt(shoot.PIN, ISR, FALLING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (!digitalRead(shoot.PIN)){
-    shoot.estado = true;
-    if (digitalRead(shoot.PIN) && shoot.estado){
-      Serial.println(shoot.caracter);
-      shoot.estado = false;
-    }
-  }
 
-  //Que letra debe enviar
+
+void loop() {
+  Joy_function(); 
+
+
+  if (digitalRead(shoot.PIN) && shoot.estado){
+    Serial.println(shoot.caracter);
+    shoot.estado = false;
+  }
+  //3.3v > push > pin boton, resistencias > gnd
+}
+
+void input_setup (){
+  pinMode(shoot.PIN, INPUT_PULLUP);
+  pinMode(ejeX.PIN, INPUT);
+  pinMode(ejeY.PIN, INPUT);
+}
+
+void Joy_function () {
+  //lee los valores en eje X y X
   ejeX.volt = analogRead (ejeX.PIN);
-  ejeY.volt = analogRead (ejeY.PIN); //valores en X y Y
-  
+  ejeY.volt = analogRead (ejeY.PIN);
+
+  //Letra correspondiente de acuerdo a donde se encuentre la palanca
   if (ejeX.volt > 2500){
     Serial.print ("D");
   }
@@ -59,19 +65,4 @@ void loop() {
   if (ejeY.volt >2500){
     Serial.print ("S");
   }
-
-  
 }
-
-void input_setup (){
-  pinMode(shoot.PIN, INPUT_PULLUP);
-  pinMode(ejeX.PIN, INPUT);
-  pinMode(ejeY.PIN, INPUT);
-}
-
-//void interrupt_setup (){
-//  attachInterrupt(up.PIN, isr, FALLING);
-//  attachInterrupt(left.PIN, isr, FALLING);
-//  attachInterrupt(down.PIN, isr, FALLING);
-//  attachInterrupt(right.PIN, isr, FALLING);
-//}
